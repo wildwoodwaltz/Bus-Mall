46102b608ffb5eac0b4181cc78ce48175a7f3a76
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-02-21 14:26:54 -0800

    added checker for duplicates for this round and previous round

--- js/app.js
+++ js/app.js
@@ -3,9 +3,13 @@
 let imgOne = document.getElementById(img-one);
 let imgTwo = document.getElementById(img-two);
 let imgThree = document.getElementById(img-three);
+let randNumberOne =[];
+let randNumberTwo=[];
+let randNumArrayOne=true;
+
 //data storage
 let allItems = []
-let itemList = ['bag','banana','bathroom','boots','breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', '' ]
+let itemList = ['bag','banana','bathroom','boots','breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors','shark','tauntaun','unicorn', 'water-can','wine-glass']
 // Constructor
 
 function Items(itemName, fileExtension = 'jpg'){
@@ -18,33 +22,63 @@ function Items(itemName, fileExtension = 'jpg'){
 }
 
 //img list
-function renderNewItems();
-new Item =('bag');
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-new Item =
-
-
-function randomItemNumber(min, max){
-  Math.round(Math.random())
+function renderNewItems(newItem);
+for (let i = 0; i < itemList.length; i++){
+  new Item = (itemList[i])
 }
+new Item = (sweep, .png)
 
+
+function getNumbers();
+  if (randNumArrayOne === true){
+    for (i = 0; i < 3; i++){
+   randNumberOne[i] = Math.floor(Math.random() * itemList.length -1);
+  }
+  console.log(randNumberOne);
+  numberChecker();
+  randNumArrayOne = false;
+  }
+  else {
+    for (i = 0; i < 3; i++){
+    randNumberTwo[i] = Math.floor(Math.random() * itemList.length -1);
+  }
+  console.log(randNumberTwo);
+  numberChecker();
+  randNumArrayOne = true;
+}
+//Check arrays vs. themselves and the other
+function numberChecker(){
+  if (randNumberOne[0] === randNumberOne [1] || randNumberOne[0] === randNumberOne[2] || randNumberOne[1] === randNumberOne[2]){
+  getNumbers();
+  }
+  else if (randNumberTwo[0] === randNumberTwo [1] || randNumberTwo[0] === randNumberTwo[2] || randNumberTwo[1] === randNumberTwo[2]){
+    getNumbers();
+  }
+  else if (randNumberOne[0] === randNumberTwo [0] || randNumberOne[0] === randNumberTwo[1] || randNumberOne[0] === randNumberTwo[2]){
+  getNumbers();
+}
+  else if (randNumberOne[1] === randNumberTwo [0] || randNumberOne[1] === randNumberTwo[1] || randNumberOne[1] === randNumberTwo[2]){
+    getNumbers();
+  }
+  else if (randNumberOne[2] === randNumberTwo [0] || randNumberOne[2] === randNumberTwo[1] || randNumberOne[2] === randNumberTwo[2]){
+  getNumbers();
+  }
+  else{
+    console.log(`Passed Check`)
+  }
 function renderImgs(){
   let imgOneIndex = randomItemNumber();
   let imgTwoIndex = randomItemNumber();
   let imgThreeIndex = randomItemNumber();
+
+
+  // Log imgs into array 1 for checking against each other
+
+  // check numbers against array 2 and display
+
+  // swap to array one
+
+
 
 while (imgOneIndex === imgTwoIndex){
   imgTwoIndex = randomItemNumber();
@@ -58,7 +92,7 @@ while (imgTwoIndex === imgThreeIndex){
   allItems[imgOneIndex].shown++
 
   imgTwo.src = allItems[imgTwoIndex].src;
-  imgOne.alt = allitems[imgTwoIndex].name;
+  imgTwo.alt = allitems[imgTwoIndex].name;
   allItems[imgTwoIndex].shown++
 
   imgThree.src = allItems[imgThreeIndex].src;

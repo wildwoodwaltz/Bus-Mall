7e2a56680e89d770f1a8181bb31356a862f89501
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-02-21 11:04:36 -0800

    created function and global variables

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// Global Variables
+
+let imgOne = document.getElementById(img-one);
+let imgTwo = document.getElementById(img-two);
+let imgThree = document.getElementById(img-three);
+//data storage
+let allItems = []
+let itemList = ['bag','banana','bathroom','boots','breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', '' ]
+// Constructor
+
+function Items(itemName, fileExtension = 'jpg'){
+  this.itemName = itemName;
+  this.src = `img/${itemName}.${fileExtension}`;
+  this.alt = `${itemName}`;
+  this.results = 0;
+  this.clicks = 0;
+  allItems.push(this);
+}
+
+//img list
+function renderNewItems();
+new Item =('bag');
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+new Item =
+
+
+function randomItemNumber(min, max){
+  Math.round(Math.random())
+}
+
+function renderImgs(){
+  imgOne.src = allItems[].src;
+  imgTwo.src = allItems[].src;
+}
+
+
+render
